923ed6d263d0b4191dac22df4c8f7c8dc303e83a
BLACKVENOM499 <blackvenomx499@gmail.com>
2026-03-09 19:51:05 +0530

    Update import statement for baileys module

--- pair.js
+++ pair.js
@@ -23,7 +23,7 @@ const {
   jidNormalizedUser,
   downloadContentFromMessage,
   DisconnectReason
-} = require('baileyz');
+} = require('@whiskeysockets/baileys');
 
 // ---------------- CONFIG ----------------
 const BOT_NAME_FREE = 'ᴀᴋɪɴᴅᴜ-ᴍᴅ';
